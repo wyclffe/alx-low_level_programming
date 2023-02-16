a46dc6b64f553f4c60d970740c0c60a582618b5b
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-16 21:20:21 +0000

    update 5 printf2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
- /**
+
+/**
   * main - this is the main function
   *
   * Return: 0 when succefull
   */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
