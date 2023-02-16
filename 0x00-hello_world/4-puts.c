ff03ec6a574c0d836d8fa7dfa91ca0dc62343306
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-16 20:09:25 +0000

    update 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *    * Return: 0
- *     */
+ * main - this is the main function
+ *   
+ * Return: always  0
+ */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
