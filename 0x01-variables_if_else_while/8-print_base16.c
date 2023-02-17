7aa790c6af1b6cba2e74e2830cb84ab4c033e770
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-17 17:33:51 +0000

    hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*  main - start  point
+* Description: print a to z
+* Return: 0
+*/
+		int main(void)
+	{
+		char c;
+		char d = '0';
+
+			while (d <= '9')
+		{
+			putchar(d);
+			d++;
+			}
+			for (c = 'a'; c <= 'f'; c++)
+		{
+				putchar(c);
+			}
+		putchar('\n');
+		return (0);
+	}
