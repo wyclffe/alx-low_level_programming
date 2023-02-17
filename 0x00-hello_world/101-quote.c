6d29fcb979951c05121bade91c0029786c226336
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-17 08:13:57 +0000

    8. UNIX update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,12 +13,14 @@ int main(void)
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
+
 	__asm__ ("syscall"
 			: "=a" (ret)
 			: "a" (syscall),
 			"D" (fd),
 			"S" (s),
 			"d" (l));
+
 	printf("");
 	return (1);
 }
