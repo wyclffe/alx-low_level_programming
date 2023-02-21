e797ad3e2793027190230fb33ca476c4335721e7
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-21 06:03:22 +0000

    putchar a string

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
  * main - prints _putchar
@@ -7,6 +7,14 @@
  */
 		int main(void)
 	{
-		printf("_putchar\n");
+		char str[] = "_putchar";
+
+		int i = 0;
+
+		for ( ; i < '\n'; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
 		return (0);
 	}
