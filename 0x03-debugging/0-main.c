dcb91420a00dfc4f8aec69ec81654aa95763e6d3
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-22 12:41:54 +0300

    debug positive_negative func

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - test positive and negative with 0
+*
+* Return: Always 0.
+*/
+		int main(void)
+	{
+		int i = 0;
+
+		positive_or_negative(i);
+
+		return (0);
+	}
