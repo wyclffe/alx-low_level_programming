1cdf44b21ba2d964937d9934db41fbc5a6029e2f
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-23 14:15:47 +0300

    10. Triangles update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - make diagonal
+ * @n: nuumber of \
+ * Return: void
+*/
+		void print_diagonal(int n)
+	{
+		char i = 0;
+
+		if (n > 0)
+		{
+			while (i < n)
+			{
+
+				for(; i < n-1 ; i++ )
+				{
+					_putchar(' ');
+
+				}
+					_putchar('\');
+
+				i++;
+			}
+			_putchar('\n');
+		}
+		else
+			putchar('\n');
+
+	}
