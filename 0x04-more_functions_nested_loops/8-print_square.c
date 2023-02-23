12055f32b9970a5a966fa158ca9381e978da4c02
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-23 13:27:07 +0300

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - make a square
+ * @size: length of square
+ * Return: void
+*/
+	void print_square(int size)
+{
+		int i, j = 0;
+
+		for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+		putchar('\n');
+}
