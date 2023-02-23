6fbd3ce8fe02ab5b7db560dbccab23ebaa7f8bb3
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-23 22:05:02 +0300

    fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,24 @@
 	{
 		int i = 0, j;
 
-			while (i < n && n > 0)
+		while (i < n && n > 0)
 			{
+			if (n == 0 && n < 0)
+			{
+				_putchar('\n');
+			}
+			else
+				{
 				 j = 0;
 
 				while (j < i)
 				{
 					_putchar(' ');
-
 					j++;
-
 				}
-					_putchar('\\');
-					_putchar('\n');
-					i++;
+				_putchar('\\');
+				_putchar('\n');
+				i++;
+				}
 			}
-
-		if (n == 0)
-			_putchar('\n');
-
 	}
