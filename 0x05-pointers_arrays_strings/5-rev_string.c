02bd8c92ee4db442e5a989d2b62e29a7d3cdbfb1
Wycliffe Ogeto <owyclffe@gmail.com>
2023-02-27 17:34:47 +0300

    half lines

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*rev_string - funtion to print string from pointer in reverse
+*@s: a pointer with a string
+* Return: void
+*/
+	void rev_string(char *s)
+{
+	int i = 10;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*puts2 - funtion to print string from pointer every other char
+*@str: an array of char
+* Return: void
+*/
+	void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+
+		i = i + 2;
+	}
+	_putchar('\n');
+}
