66eb15d884db5c1c81372aabfffad1e37d3d8265
Wycliffe Ogeto <owyclffe@gmail.com>
2023-03-02 17:17:30 +0300

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,14 @@
 	char *_strncat(char *dest, char *src, int n)
 	{
 	int dest_len = 0, i = 0;
-	
+
 	while (dest[i++])
 	{
 		dest_len++;
 	}
 	for (i = 0 ; i < n && src[i] ; i++)
 	{
-		dest[dest_len ++] = src[i];
+		dest[dest_len++] = src[i];
 	}
 		return (dest);
 	}
