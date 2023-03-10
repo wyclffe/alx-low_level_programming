31dfc39577525be8c060ab2d47946e20cbcf0e0f
Wycliffe Ogeto <owyclffe@gmail.com>
2023-03-10 22:18:46 +0300

    sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
-* main - Prints all arguments passed into main
+* main - Program that takes first two integer arguments and prints the product
 * @argc: Number of command line arguments
 * @argv: Array name
-* Return: 0
+* Return: 1 if not enough arguments passed in, 0 otherwise
 */
 
 int main(int argc, char *argv[])
 {
-int i;
+	int i, j;
 
-for (i = 0; i < argc; i++)
-printf("%s\n", argv[i]);
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
 
-return (0);
+		for (i = 1; i < 3; i++)
+		j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
 }
